fc903d1617ef4ffaa387cdb79430df1ed5562c3d
Victor Afanassieff <victor@afanassieff.com>
2017-06-13 13:09:59 +0200

    Weapon name function changed

--- sources/wolf_weapon.c
+++ sources/wolf_weapon.c
@@ -6,13 +6,13 @@
 /*   By: vafanass <vafanass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/11 19:55:28 by vafanass          #+#    #+#             */
-/*   Updated: 2017/06/13 12:51:15 by vafanass         ###   ########.fr       */
+/*   Updated: 2017/06/13 13:09:21 by vafanass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-void    weapon_anim(t_wolf *wolf, int nb)
+void    weapon_to_surface(t_wolf *wolf, int nb)
 {
 	uint32_t		color;
     register int 	x;
@@ -30,39 +30,39 @@ void    weapon_anim(t_wolf *wolf, int nb)
 		}
 	}
 }
-void    weapon_choice(t_wolf *wolf)
+void    weapon_anim(t_wolf *wolf)
 {
     static int nb;
     
     if (nb > 50 || nb < 0)
         nb = 0;
     if (nb <= 7 && nb > 0)
-        weapon_anim(wolf, 1);
+        weapon_to_surface(wolf, 1);
     else if (nb <= 14 && nb >= 7)
-        weapon_anim(wolf, 2);
+        weapon_to_surface(wolf, 2);
     else if (nb <= 21 && nb >= 14)
-        weapon_anim(wolf, 3);
+        weapon_to_surface(wolf, 3);
     else if (nb <= 28 && nb >= 21)
-        weapon_anim(wolf, 4);
+        weapon_to_surface(wolf, 4);
     else if (nb <= 35 && nb >= 28)
-        weapon_anim(wolf, 5);
+        weapon_to_surface(wolf, 5);
     else if (nb <= 42 && nb >= 35)
-        weapon_anim(wolf, 4);
+        weapon_to_surface(wolf, 4);
     else if (nb <= 49 && nb >= 42)
-        weapon_anim(wolf, 3);
+        weapon_to_surface(wolf, 3);
     else if (nb == 50)
     {
         wolf->fire = 0;
-        weapon_anim(wolf, 0);
+        weapon_to_surface(wolf, 0);
     }
     else
-         weapon_anim(wolf, 0);
+         weapon_to_surface(wolf, 0);
     nb++;
 }
 void    display_weapon(t_wolf *wolf)
 {
     if (wolf->fire == 1)
-        weapon_choice(wolf);
+        weapon_anim(wolf);
     else
-        weapon_anim(wolf, 0);
+        weapon_to_surface(wolf, 0);
 }
